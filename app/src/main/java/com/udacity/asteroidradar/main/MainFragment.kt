b053ac748218e9d968b443b2155367a04ee7edb9
mo <mo@gmail.com>
2022-12-11 21:05:41 +0200

    initial commit

--- app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
@@ -3,7 +3,9 @@ package com.udacity.asteroidradar.main
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.udacity.asteroidradar.R
 import com.udacity.asteroidradar.databinding.FragmentMainBinding
 
@@ -16,9 +18,21 @@ class MainFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val binding = FragmentMainBinding.inflate(inflater)
+
         binding.lifecycleOwner = this
 
         binding.viewModel = viewModel
+
+        binding.asteroidRecycler.adapter = AsteroidAdapter(AsteroidAdapter.AsteroidListener {
+            viewModel.displayAsteroidDetails(it)
+        })
+
+        viewModel.navigateToSelectedAsteroid.observe(viewLifecycleOwner, Observer {
+            if ( null != it ) {
+                this.findNavController().navigate(MainFragmentDirections.actionShowDetail(it))
+                viewModel.displayAsteroidDetailsComplete()
+            }
+        })
 
         setHasOptionsMenu(true)
 
--- app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -1,13 +1,64 @@
 package com.udacity.asteroidradar.main
 
 import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import com.udacity.asteroidradar.Asteroid
+import com.udacity.asteroidradar.api.AsteroidApi
 import com.udacity.database.AsteroidsDatabaseDao
+import kotlinx.coroutines.launch
+
+enum class ASteroidApiStatus { LOADING, ERROR, DONE }
 
 class MainViewModel (
         val database: AsteroidsDatabaseDao,
         application: Application) : AndroidViewModel(application){
+
+        private val _status = MutableLiveData<ASteroidApiStatus>()
+        val status: LiveData<ASteroidApiStatus>
+                get() = _status
+
+        private val _asteroids = MutableLiveData<List<Asteroid>>()
+        val asteroids: LiveData<List<Asteroid>>
+                get() = _asteroids
+
+//        val myList = listOf<Asteroid>(
+//                Asteroid(1,"dsf","fdsf", 1.0, 4.3, 5.1, 4.3, false),
+//                Asteroid(1,"dsf","fdsf", 1.0, 4.3, 5.1, 4.3, false),
+//                Asteroid(1,"dsf","fdsf", 1.0, 4.3, 5.1, 4.3, false),
+//                Asteroid(1,"dsf","fdsf", 1.0, 4.3, 5.1, 4.3, false),
+//                Asteroid(1,"dsf","fdsf", 1.0, 4.3, 5.1, 4.3, false),
+//                Asteroid(1,"dsf","fdsf", 1.0, 4.3, 5.1, 4.3, false)
+//                )
+
+        private val _navigateToSelectedAsteroid = MutableLiveData<Asteroid>()
+
+        val navigateToSelectedAsteroid: LiveData<Asteroid>
+                get() = _navigateToSelectedAsteroid
+
+
+        init {
+            getNASAAsteroids()
+        }
+        private fun getNASAAsteroids() {
+                viewModelScope.launch {
+                        _status.value = ASteroidApiStatus.LOADING
+                        try {
+                                _asteroids.value = AsteroidApi.retrofitService.getProperties()
+                                _status.value = ASteroidApiStatus.DONE
+                        } catch (e: Exception) {
+                                _status.value = ASteroidApiStatus.ERROR
+                                _asteroids.value = ArrayList()
+                        }
+                }
+        }
+
+        fun displayAsteroidDetails(asteroidDetails: Asteroid) {
+                _navigateToSelectedAsteroid.value = asteroidDetails
+        }
+
+        fun displayAsteroidDetailsComplete() {
+                _navigateToSelectedAsteroid.value = null
+        }
 
 
 }
