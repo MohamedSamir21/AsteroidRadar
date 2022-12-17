4117e7011c68390f36f94fff72750412ba22f1b0
mo <mo@gmail.com>
2022-12-17 04:17:22 +0200

    initial commit

--- app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -1,7 +1,8 @@
 package com.udacity.asteroidradar
 
 object Constants {
-    const val API_KEY = "gCwa5SHxwetinsEAoxL9ZP2XABjrtcCJBvdO90pK"
+    //Put your API Key between the double quotes below.
+    const val API_KEY = ""
     const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
